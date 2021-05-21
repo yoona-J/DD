f87957459ed7df7ff3bafff67b27b13e36ba293a
Your Name <you@example.com>
2021-05-22 00:48:14 +0900

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose  = require('mongoose')
+mongoose.connect('mongodb+srv://yoona:nayoo@yoona.lllx3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
